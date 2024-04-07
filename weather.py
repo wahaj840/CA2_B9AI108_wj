5282bb72e940c45e8e1bca839395469cb2b2198c
wahaj840 <59430560+wahaj840@users.noreply.github.com>
2024-04-07 23:39:04 +0100

    virtual env. has been set up successfully and all the packages have been imported in it

--- weather.py
+++ weather.py
@@ -1,5 +1,9 @@
 import requests
+from dotenv import load_dotenv
+import os
 
-def get_lang_long(city_name, state_code, country_code, limit, API_key):
-    response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&limit={limit}&appid={API_key}')
+def get_lang_long(city_name, state_code, country_code, API_key):
+    response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}').json()
+
+    print(response)
     
