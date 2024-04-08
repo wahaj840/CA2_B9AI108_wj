88f42456e01258eb54e3a2a54bf329f7574096fe
wahaj840 <59430560+wahaj840@users.noreply.github.com>
2024-04-09 00:15:43 +0100

    Additional Features added in the WeatherData

--- weather.py
+++ weather.py
@@ -13,6 +13,12 @@ class WeatherData:
     description: str
     icon: str
     tempreture: float
+    min_tempreture: float
+    max_tempreture: float
+    air_pressure: int
+    humidity: int
+    
+
 
 def get_lang_long(city_name, state_code, country_code, API_key):
     response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}').json()
@@ -28,19 +34,27 @@ def get_lang_long(city_name, state_code, country_code, API_key):
 
 def get_current_weather(lat, lon, API_key):
     response= requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}&units=metric').json()
-    #print (response)
+   
     data= WeatherData(
         main=response.get('weather')[0].get('main'),
         description=response.get('weather')[0].get('description'),
         icon=response.get('weather')[0].get('icon'),
-        tempreture=response.get('weather')[0].get('temp'),
+        tempreture=response.get('main').get('temp'),
+        min_tempreture=response.get('main').get('temp_min'),
+        max_tempreture=response.get('main').get('temp_max'),
+        air_pressure=response.get('main').get('pressure'),
+        humidity=response.get('main').get('humidity')
+
         )
+    
+    return data
 
 
 if __name__=="__main__":
     lat ,lon = get_lang_long('Dublin','Leinster', 'Ireland', api_key)
 
-    get_current_weather(lat, lon, api_key)
+    print(get_current_weather(lat, lon, api_key))
+
 
 
 
