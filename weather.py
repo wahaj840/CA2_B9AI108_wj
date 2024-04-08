c9e64f76a7e2b3ec600476289b9b2c617240eae9
wahaj840 <59430560+wahaj840@users.noreply.github.com>
2024-04-08 02:57:10 +0100

    Code tested with USA and Dublin Data & organized to print Latitude and Longitude

--- weather.py
+++ weather.py
@@ -7,8 +7,13 @@ api_key=os.getenv('API_KEY')
 
 def get_lang_long(city_name, state_code, country_code, API_key):
     response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}').json()
+   
+    data = response[0]
+    lat, lon =data.get('lat'), data.get('lon')
+    return lat, lon
 
-    print(response)
+    #print(response)
 
-get_lang_long('Houston','Texas', 'United States', api_key)
-        
+#get_lang_long('Dublin','Dublin','Ireland', api_key)
+
+print(get_lang_long('Dublin','Dublin', 'Ireland', api_key))        
