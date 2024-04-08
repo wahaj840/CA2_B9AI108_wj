5a65f0d7f6aefddd5cb7d7777b21ff1c773d67c3
wahaj840 <59430560+wahaj840@users.noreply.github.com>
2024-04-08 01:45:15 +0100

    .json format added in the response to convert response into valid jason obj

--- weather.py
+++ weather.py
@@ -6,7 +6,7 @@ load_dotenv
 api_key=os.getenv('API_KEY')
 
 def get_lang_long(city_name, state_code, country_code, API_key):
-    response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}')
+    response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}').json()
 
     print(response)
 
