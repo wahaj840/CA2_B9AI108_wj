ca6e58e805da18cc34013accde0ba4e792767814
wahaj840 <59430560+wahaj840@users.noreply.github.com>
2024-04-08 01:41:37 +0100

    Got the response [200] from the website

--- weather.py
+++ weather.py
@@ -2,8 +2,13 @@ import requests
 from dotenv import load_dotenv
 import os
 
+load_dotenv
+api_key=os.getenv('API_KEY')
+
 def get_lang_long(city_name, state_code, country_code, API_key):
-    response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}').json()
+    response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}')
 
     print(response)
-    
+
+get_lang_long('Houston','Texas', 'United States', api_key)
+        
