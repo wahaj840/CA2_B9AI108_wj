34008026722fec4b026021d64f768742ec05f1cf
wahaj840 <59430560+wahaj840@users.noreply.github.com>
2024-04-09 01:17:14 +0100

    @app.route defined for mapping the URL to a specific function

--- weather_app.py
+++ weather_app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, request
 
 from weather import main as get_weather
 
+app= Flask(__name__)
 
-app=Flask(__name__)
-
-
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__=='__main__':
     app.run(debug=True)
