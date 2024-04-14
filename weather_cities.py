d3cc32f639d215ef7080067d3ac74a54d45db573
wahaj840 <59430560+wahaj840@users.noreply.github.com>
2024-04-14 08:03:05 +0100

    Defining the URL where we will be fetching the data, its units and the API key

--- weather_cities.py
+++ weather_cities.py
@@ -5,13 +5,15 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-import sys
+import sys                                 # This package will output the nearest city to a specific pair of geographical  coordinates. It is very important for this project.
 import citipy
 from citipy import citipy
 
 
-
+#Defining the URL where we will be fetching the data, its units and the API key
+url="http://api.openweathermap.org/data/2.5/weather?"
+units ='metric'
+api_key="c542897b543bb199a66f36f6de4e89c5"
 
 
 
