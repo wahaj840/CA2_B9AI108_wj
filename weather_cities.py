02e0b0c4f03f8158c1c0fde5ad94ebc54891c70f
wahaj840 <59430560+wahaj840@users.noreply.github.com>
2024-04-14 08:06:29 +0100

    Latitude and Longitude limits set for Ireland

--- weather_cities.py
+++ weather_cities.py
@@ -16,6 +16,20 @@ units ='metric'
 api_key="c542897b543bb199a66f36f6de4e89c5"
 
 
+#As we will be analysing data of different cities of Ireland so i have defined the latitude and longitude for Ireland baseed on which we will be pulling the data from the website Via API
+
+# Latitude limits for Ireland
+lat_min= 51.5
+lat_max=55.5
+
+# Longitude limits for Ireland
+lon_min= -11.5
+lon_max=-6
+
+
+
+
+
 
 
 
