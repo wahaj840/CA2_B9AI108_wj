c02367f7cd2fc92c0f3d3dbef7865080f6cf9d14
wahaj840 <59430560+wahaj840@users.noreply.github.com>
2024-04-14 11:38:27 +0100

    Fetch Raw Data

--- raw_weather_data.py
+++ raw_weather_data.py
@@ -3,30 +3,30 @@ import random
 import pandas as pd
 from citipy import citipy
 
-def fetch_raw_data(api_key):
-    url = "http://api.openweathermap.org/data/2.5/weather?"
-    units ='metric'
-    api_key="c542897b543bb199a66f36f6de4e89c5"
+url = "http://api.openweathermap.org/data/2.5/weather?"
+units = 'metric'
+api_key = "c542897b543bb199a66f36f6de4e89c5"
 
-
-'''Copying code from weather_cities file'''
-# Latitude limits for Ireland & UK  
-lat_min= 50
-lat_max=60
-
-# Longitude limits for Ireland & UK
-lon_min= -10.5
-lon_max= 2
-
-# Setting a counter and create an empty list for holding the city and country information
-counter=0
-cities=[]
-
-while counter<50:   #Generating random lat and lon cordinates w.r.t the assigned range
-    lat =random.uniform(lat_min, lat_max)
-    lon=random.unform(lon_min, lon_max)
-    city = citipy.nearest_city(lat, lon).city_name
-    if city not in cities:
-     cities.append(city)
-     counter+=1
+def fetch_raw_weather_data():
+    lat_min = 50
+    lat_max = 60
+    lon_min = -10.5
+    lon_max = 2
+    cities = []
+    counter = 0
+    while counter < 50:
+        lat = random.uniform(lat_min, lat_max)
+        lon = random.uniform(lon_min, lon_max)
+        city = citipy.nearest_city(lat, lon).city_name
+        if city not in cities:
+            cities.append(city)
+            counter += 1
+    weather_data = []
+    for city in cities:
+        try:
+            response = requests.get(f"{url}appid={api_key}&units={units}&q={city}").json()
+            weather_data.append(response)
+        except Exception as e:
+            print(f"Error occurred for {city}: {e}")
+    return weather_data
 
